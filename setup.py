cb9cab0bb14ac12faff2ae7ec17155a24a739aa7
David Seifert <dseifert@pacificbiosciences.com>
2018-08-14 23:26:33 +0200

    Modernize setup.py

--- setup.py
+++ setup.py
@@ -10,25 +10,26 @@ from os.path import join, dirname
 # make integration unnecessarily hard
 
 setup(
-    name = 'GenomicConsensus',
-    version='2.3.1', # don't forget to update GenomicConsensus/__init__.py and doc/conf.py too
+    name='GenomicConsensus',
+    version='2.3.1',  # don't forget to update GenomicConsensus/__init__.py and doc/conf.py too
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license=open('LICENSES').read(),
-    scripts = ['bin/variantCaller',
-               'bin/summarizeConsensus',
-               'bin/gffToVcf',
-               'bin/gffToBed',
-               'bin/plurality',
-               'bin/poa',
-               'bin/quiver',
-               'bin/arrow'],
-    packages = find_packages(),
-    package_data={'GenomicConsensus.quiver': ['resources/*/GenomicConsensus/*.ini']},
+    scripts=[
+        'bin/variantCaller',
+        'bin/summarizeConsensus',
+        'bin/gffToVcf',
+        'bin/gffToBed',
+        'bin/plurality',
+        'bin/poa',
+        'bin/quiver',
+        'bin/arrow'],
+    packages=find_packages(),
     include_package_data=True,
-    zip_safe = False,
+    zip_safe=False,
+    setup_requires=['setuptools'],
     install_requires=[
         'pbcore >= 1.2.9',
         'pbcommand >= 0.3.20',
         'numpy >= 1.6.0']
-    )
+)
