f61c7b3cd73c8af6149524f8d95d818371a995c9
David Seifert <dseifert@pacificbiosciences.com>
2018-08-14 23:26:34 +0200

    Version bump to 2.3.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os.path import join, dirname
 
 setup(
     name='GenomicConsensus',
-    version='2.3.1',  # don't forget to update GenomicConsensus/__init__.py and doc/conf.py too
+    version='2.3.2',  # don't forget to update GenomicConsensus/__init__.py and doc/conf.py too
     author='Pacific Biosciences',
     author_email='devnet@pacificbiosciences.com',
     license=open('LICENSES').read(),
